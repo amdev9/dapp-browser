3a896cc74a3b349f9cd27a6a983608db86822e6d
alf <alfmohenu@outlook.com>
2017-12-21 16:56:29 +0300

    Data from Manifest file extracted

---
+++ renderer.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const fs = require("fs");
+const dir = require("node-dir");
+
+const dirpath = path.join(__dirname, "/dapps/");
+
+const NameAndPermissions = {};
+
+function clicked(id) {
+  let appPath = dirpath + id;
+  dir.readFiles(
+    appPath,
+    {
+      match: /manifest.json$/
+    },
+    function(err, content, next) {
+      if (err) throw err;
+      let parsedContent = JSON.parse(content);
+      NameAndPermissions.name = parsedContent["name"];
+      NameAndPermissions.permissions = parsedContent["permissions"]
+      module.exports = NameAndPermissions;
+      console.log(NameAndPermissions);
+      next();
+    },
+    function(err, files) {
+      if (err) throw err;
+      console.log("finished reading files:", files);
+    }
+  );
+}
