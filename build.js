e51044fee57680b4f4ba3758adc48455c3e072fb
Webkostya <webkostya@icloud.com>
2018-03-29 18:49:22 +0300

    Add NodeJS

--- build.js
+++ build.js
@@ -1,7 +1,8 @@
+// nwb nwbuild -v 0.29.3-sdk -p osx64 ./build/ --include ./:./*
 const NwBuilder = require( 'nw-builder' )
 
-const include = ['./dapps/**', './*.js', './*.json', './*.html', './*.plist']
-const exclude = ['!./build.js', '!./app.js', '!./index.html', '!./package-lock.json', '!./blockchain.json']
+const include = ['./dapps/**', './*.js', './*.json', './*.html', './*.plist', './bin/**', './assets/**', './components/**', './database/**', './library/**', './logs/**', './routes/**', './views/**', './array', './node_modules/**']
+const exclude = ['!./build.js','!./package-lock.json']
 const files = include.concat( exclude )
 
 const object = new NwBuilder({
@@ -9,8 +10,9 @@ const object = new NwBuilder({
     files: files,
     platforms: ['osx64'],
     buildDir: './dist',
-    flavor: 'sdk', // normal
-    macPlist: './Info.plist'
+    flavor: 'normal', // normal
+    macPlist: './Info.plist',
+    production: true
 })
 
 object.build(function ( error ) {
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const child_process = require( 'child_process' )
-child_process.exec('node ./bin/www')
+const gui = require( 'nw.gui' )
+
+const child = child_process.exec( './array ./bin/www' )
+    
+child.on('exit', () => {
+    alert( 'process exit' )
+})
+
+// alert(gui.App.argv)
 
 nw.Window.open('http://localhost:3000/', {
     title: 'Welcome',
     width: 1000,
     height: 650,
     frame: true, // false: disable toolbar
+    // nodejs: false,
 }, target => target.setMinimumSize(1000, 650))
