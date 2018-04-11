fab1df133577e3f5bfd317fca460083159c13f11
Igor Lavrenov <lavrenov@users.noreply.github.com>
2018-04-11 18:26:04 +0300

    Create build.win.js

---
+++ build.win.js
@@ -0,0 +1,19 @@
+const NwBuilder = require( 'nw-builder' )
+
+const include = ['./**', './*.js', './*.json', './*.html', './*.plist']
+const exclude = ['!./build.js','!./package-lock.json', '!./cache/**', '!./dist/**']
+const files = include.concat( exclude )
+
+const object = new NwBuilder({
+    appName: 'Array.IO',
+    files: files,
+    platforms: ['win64'],
+    buildDir: './dist',
+    flavor: 'sdk', // normal
+    macPlist: './Info.plist'
+})
+
+object.build(function ( error ) {
+    if ( error ) return console.error( error )
+    console.log('Complete !')
+})
