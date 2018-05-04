c0cd8526aa5cbb51da0ce4ad815426d06c882e00
Webkostya <webkostya@icloud.com>
2018-05-04 15:32:42 +0300

    Drag&Drop

--- dapps/users/drop919281257128/assets/js/index.js
+++ dapps/users/drop919281257128/assets/js/index.js
@@ -7,7 +7,34 @@
     const output = document.getElementById( 'output' )
     const confirm = document.getElementById( 'confirm' )
     const progress = document.getElementById( 'progress' )
+    const dragdrop = document.getElementById( 'dragdrop' )
 
+
+    // Drag And Drop
+    document.addEventListener('dragover', function ( event ) {
+        event.preventDefault()
+        document.body.classList.add( 'dragover' )
+    })
+
+    dragdrop.addEventListener('drop', function ( event ) {
+        event.preventDefault()
+        document.body.classList.remove( 'dragover' )
+
+        const files = event.dataTransfer.files
+        const data = new FormData()
+
+        Array.from( files ).forEach(element => data.append('choose', element))
+
+        Uploader( data )
+    })
+
+    dragdrop.addEventListener('dragleave', function () {
+        event.preventDefault()
+        document.body.classList.remove( 'dragover' )
+    })
+
+
+    // Choose File
     choose.addEventListener('change', function () {
         let array = []
         
@@ -19,14 +46,21 @@
         label.innerText = array.join( ', ' )
     })
     
+
+    // Submit Upload Form
     upload.addEventListener('submit', function ( event ) {
         event.preventDefault()
 
-        output.innerHTML = ''
+        if ( !this.transfer.value.length ) return
+        
+        const data = new FormData( this )
+        Uploader( data )
+    })
 
-        let data = new FormData( this )
-        let xhr  = new XMLHttpRequest()
-    
+
+    // Upload Method
+    const Uploader = function ( data ) {
+        const xhr  = new XMLHttpRequest()
         xhr.open('post', '/transfer')
 
         xhr.upload.addEventListener('progress', function ( event ) {
@@ -60,5 +94,5 @@
         }
 
         xhr.send( data )
-    })
+    }
 })();
