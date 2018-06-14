7db79805957841049ffaac56abbb270575329188
cypherpunk99 <amatveevdev9@gmail.com>
2018-06-14 17:22:13 +0300

    browserify on client side? or compine in preload

---
+++ electron/browserview/index.js
@@ -0,0 +1,23 @@
+// *** working with redux, get and post data to ipc through
+
+var assert = require('assert');
+var redux = require('redux');
+var createStore = redux.createStore;
+
+var reducer = function(state, action) {
+  if (!state) state = 0;
+  if (action.type === 'INC') return state + 1;
+  return state;
+}
+
+var store = createStore(reducer);
+
+assert.equal(store.getState(), 0)
+
+store.dispatch({type: 'INC'});
+
+assert.equal(store.getState(), 1);
+
+if (global.document) {
+  document.write('My number is ' + store.getState());
+}
