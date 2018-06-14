748aaa540be3ad8ac24a213eb1ba873947963c9a
cypherpunk99 <amatveevdev9@gmail.com>
2018-06-14 18:43:21 +0300

    clientside latest

--- electron/browserview/index.js
+++ electron/browserview/index.js
@@ -1,23 +1,109 @@
 // *** working with redux, get and post data to ipc through
 
-var assert = require('assert');
-var redux = require('redux');
-var createStore = redux.createStore;
+// var assert = require('assert');
+// var redux = require('redux');
+// var createStore = redux.createStore;
 
-var reducer = function(state, action) {
-  if (!state) state = 0;
-  if (action.type === 'INC') return state + 1;
-  return state;
+// var reducer = function(state, action) {
+//   if (!state) state = 0;
+//   if (action.type === 'INC') return state + 1;
+//   return state;
+// }
+
+// var store = createStore(reducer);
+
+// assert.equal(store.getState(), 0)
+
+// store.dispatch({type: 'INC'});
+
+// assert.equal(store.getState(), 1);
+
+// if (global.document) {
+//   document.write('My number is ' + store.getState());
+// }
+
+//***** define redux, redux-thunk with browserify */
+const redux = electron.remote.require('redux');
+const combineReducers = redux.combineReducers;
+const createStore = redux.createStore;
+const applyMiddleware = redux.applyMiddleware;
+const compose = redux.compose;
+// { combineReducers, createStore, applyMiddleware, compose } 
+const thunk = electron.remote.require('redux-thunk').default;
+// const { hashHistory } = electron.remote.require('react-router');
+// const { routerMiddleware } = electron.remote.require('react-router-redux');
+const { isFSA } = electron.remote.require('flux-standard-action');
+
+const validateAction = (action) => {
+  if (!isFSA(action)) {
+    // log('WARNING! Action not FSA-compliant', action);
+
+    return false;
+  }
+  return true;
 }
 
-var store = createStore(reducer);
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT_COUNTER':
+      return state + 1;
+    case 'DECREMENT_COUNTER':
+      return state - 1;
+    default:
+      return state;
+  }
+}
 
-assert.equal(store.getState(), 0)
+const rootReducer = combineReducers({
+  counter
+});
 
-store.dispatch({type: 'INC'});
+const configureStore = (initialState) => {
 
-assert.equal(store.getState(), 1);
+  const middleware = [thunk, forwardToMain];
+  const enhanced = [
+    applyMiddleware(...middleware),
+  ];
+  const enhancer = compose(...enhanced);
+  
+  console.log(redux.createStore);
+  console.log(typeof rootReducer, initialState, typeof enhancer);
+  const store = createStore(rootReducer, initialState, enhancer);  
+   
+  // replayActionRenderer(store); // window.ipc
 
-if (global.document) {
-  document.write('My number is ' + store.getState());
+  return store;
+};
+
+const forwardToMain = store => next => (action) => {  
+  if (!validateAction(action)) return next(action);
+
+  if (
+    action.type.substr(0, 2) !== '@@'
+    && action.type.substr(0, 10) !== 'redux-form'
+    && (
+      !action.meta
+      || !action.meta.scope
+      || action.meta.scope !== 'local'
+    )
+  ) {
+
+    // sendToMainAction(action); // window.ipc  /**** access from preload script */ 
+      
+    // stop action in-flight
+    // eslint-disable-next-line consistent-return
+    return;
+  }
+
+  // eslint-disable-next-line consistent-return
+  return next(action);
+};
+
+const enableStore = () => {
+  
+  // const states = getGlobalState(); // window.ipc 
+  const initialState = JSON.parse(states());//getInitialStateRenderer(); // ???
+   
+  const store = configureStore(initialState);
+  return store;
 }
--- electron/browserview/preload.js
+++ electron/browserview/preload.js
@@ -2,24 +2,7 @@
 const electron = require('electron');
 const ipcRenderer = electron.ipcRenderer;
 
-/*****/
-// var redux = electron.remote.require('redux'); //equire('redux');
-// var createStore = redux.createStore;
-
-// var reducer = function(state, action) {
-//   if (!state) state = 0;
-//   if (action.type === 'INC') return state + 1;
-//   return state;
-// }
-// var store = createStore(reducer);
-
-// const unsubscribe = store.subscribe(() =>
-//   console.log('CONSOLE>LOG', store.getState())
-// );
-
-
-// store.dispatch({type: 'INC'});
-/****/
+ 
 
 // const getInitialStateRenderer = electron.remote.require('/home/pidgin/job/boilerplate/electron-redux/packages/electron-redux').getInitialStateRenderer;
 // const configureStore = electron.remote.require('./store/configureStore'); //
@@ -32,18 +15,6 @@ const ipcRenderer = electron.ipcRenderer;
 
 // const rootReducer = electron.remote.require('../reducers');
 
-
-//***** define redux, redux-thunk with browserify */
-const redux = electron.remote.require('redux');
-const combineReducers = redux.combineReducers;
-const createStore = redux.createStore;
-const applyMiddleware = redux.applyMiddleware;
-const compose = redux.compose;
-// { combineReducers, createStore, applyMiddleware, compose } 
-const thunk = electron.remote.require('redux-thunk').default;
-// const { hashHistory } = electron.remote.require('react-router');
-// const { routerMiddleware } = electron.remote.require('react-router-redux');
-const { isFSA } = electron.remote.require('flux-standard-action');
 
 
 // require = null; 
@@ -66,85 +37,23 @@ class SafeIpcRenderer {
       };
     };
 
-    const validateAction = (action) => {
-      if (!isFSA(action)) {
-        // log('WARNING! Action not FSA-compliant', action);
     
-        return false;
-      }
-      return true;
-    }
-
-
-    const forwardToMain = store => next => (action) => { // eslint-disable-line no-unused-vars
-      if (!validateAction(action)) return next(action);
-    
-      if (
-        action.type.substr(0, 2) !== '@@'
-        && action.type.substr(0, 10) !== 'redux-form'
-        && (
-          !action.meta
-          || !action.meta.scope
-          || action.meta.scope !== 'local'
-        )
-      ) {
-        ipcRenderer.send('redux-action', action); /**** access from preload script */
-    
-        // stop action in-flight
-        // eslint-disable-next-line consistent-return
-        return;
-      }
-    
-      // eslint-disable-next-line consistent-return
-      return next(action);
-    };
-
-    const counter = (state = 0, action) => {
-      switch (action.type) {
-        case 'INCREMENT_COUNTER':
-          return state + 1;
-        case 'DECREMENT_COUNTER':
-          return state - 1;
-        default:
-          return state;
-      }
-    }
-
-    const rootReducer = combineReducers({
-      counter
-      // routing
-    });
-
     const replayActionRenderer = (store) => { //*** define in preload script */
       ipcRenderer.on('redux-action', (event, payload) => {
         store.dispatch(payload);
       });
     }
 
-    const configureStore = (initialState) => {
+   
+    const getGlobalState = () => {
+      const globalState = electron.remote.getGlobal('getReduxState'); 
+      return globalState;
+    }
 
-      const middleware = [thunk, forwardToMain];
-      const enhanced = [
-        applyMiddleware(...middleware),
-      ];
-      const enhancer = compose(...enhanced);
-      
-      console.log(redux.createStore);
-      console.log(typeof rootReducer, initialState, typeof enhancer);
-      const store = createStore(rootReducer, initialState, enhancer);  // ?
-       
-      replayActionRenderer(store);
-    
-      return store;
-    };
-
-    const enableStore = () => {
-      const states = electron.remote.getGlobal('getReduxState');
-      const initialState = JSON.parse(states());//getInitialStateRenderer(); // ???
-       
-      const store = configureStore(initialState);
-      return store;
-    } // change to browserify - not load correct redux
+    const sendToMainAction = (action) => {
+      ipcRenderer.send('redux-action', action);
+    } 
+  
 
     this.on = protect(ipcRenderer.on);
     this.once = protect(ipcRenderer.once);
