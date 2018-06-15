65950e492f66ceb7452603ff16e409b51df2b325
cypherpunk99 <amatveevdev9@gmail.com>
2018-06-15 14:44:54 +0300

    works pass params

--- electron/store/configureStore.js
+++ electron/store/configureStore.js
@@ -5,52 +5,55 @@ const { hashHistory } = require('react-router');
 const { routerMiddleware } = require('react-router-redux');
 const { isFSA } = require('flux-standard-action');
 
+
+const { forwardToRenderer, replayActionMain } = require('electron-redux');
+
 const rootReducer = require('../reducers');
 
-const validateAction = (action) => {
-  if (!isFSA(action)) {
-    // log('WARNING! Action not FSA-compliant', action);
-    return false;
-  }
-  return true;
-}
+// const validateAction = (action) => {
+//   if (!isFSA(action)) {
+//     // log('WARNING! Action not FSA-compliant', action);
+//     return false;
+//   }
+//   return true;
+// }
 
-const forwardToRenderer = () => next => (action) => {
-  if (!validateAction(action)) return next(action);
-  if (action.meta && action.meta.scope === 'local') return next(action);
+// const forwardToRenderer = () => next => (action) => {
+//   if (!validateAction(action)) return next(action);
+//   if (action.meta && action.meta.scope === 'local') return next(action);
 
-  // change scope to avoid endless-loop
-  const rendererAction = {
-    ...action,
-    meta: {
-      ...action.meta,
-      scope: 'local',
-    },
-  };
+//   // change scope to avoid endless-loop
+//   const rendererAction = {
+//     ...action,
+//     meta: {
+//       ...action.meta,
+//       scope: 'local',
+//     },
+//   };
 
-  const allWebContents = webContents.getAllWebContents();
+//   const allWebContents = webContents.getAllWebContents();
 
-  allWebContents.forEach((contents) => {
-    contents.send('redux-action', rendererAction);
-  });
+//   allWebContents.forEach((contents) => {
+//     contents.send('redux-action', rendererAction);
+//   });
 
-  return next(action);
-};
+//   return next(action);
+// };
 
-const replayActionMain = (store) => {
-  /**
-   * Give renderers a way to sync the current state of the store, but be sure
-   * we don't expose any remote objects. In other words, we need our state to
-   * be serializable.
-   *
-   * Refer to https://github.com/electron/electron/blob/master/docs/api/remote.md#remote-objects
-   */
-  global.getReduxState = () => JSON.stringify(store.getState());
+// const replayActionMain = (store) => {
+//   /**
+//    * Give renderers a way to sync the current state of the store, but be sure
+//    * we don't expose any remote objects. In other words, we need our state to
+//    * be serializable.
+//    *
+//    * Refer to https://github.com/electron/electron/blob/master/docs/api/remote.md#remote-objects
+//    */
+//   global.getReduxState = () => JSON.stringify(store.getState());
 
-  ipcMain.on('redux-action', (event, payload) => {
-    store.dispatch(payload);
-  });
-}
+//   ipcMain.on('redux-action', (event, payload) => {
+//     store.dispatch(payload);
+//   });
+// }
 
 const configureStore = (initialState) => {
   const middleware = [];
