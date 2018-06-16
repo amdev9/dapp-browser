2c9f88bec98a8f94bce661aa054caa9273db4668
amatveevdev <amatveevdev9@gmail.com>
2018-06-16 15:28:17 +0300

    remove

--- electron/store/configureStore.js
+++ electron/store/configureStore.js
@@ -9,51 +9,7 @@ const { isFSA } = require('flux-standard-action');
 const { forwardToRenderer, replayActionMain } = require('electron-redux');
 
 const rootReducer = require('../reducers');
-
-// const validateAction = (action) => {
-//   if (!isFSA(action)) {
-//     // log('WARNING! Action not FSA-compliant', action);
-//     return false;
-//   }
-//   return true;
-// }
-
-// const forwardToRenderer = () => next => (action) => {
-//   if (!validateAction(action)) return next(action);
-//   if (action.meta && action.meta.scope === 'local') return next(action);
-
-//   // change scope to avoid endless-loop
-//   const rendererAction = {
-//     ...action,
-//     meta: {
-//       ...action.meta,
-//       scope: 'local',
-//     },
-//   };
-
-//   const allWebContents = webContents.getAllWebContents();
-
-//   allWebContents.forEach((contents) => {
-//     contents.send('redux-action', rendererAction);
-//   });
-
-//   return next(action);
-// };
-
-// const replayActionMain = (store) => {
-//   /**
-//    * Give renderers a way to sync the current state of the store, but be sure
-//    * we don't expose any remote objects. In other words, we need our state to
-//    * be serializable.
-//    *
-//    * Refer to https://github.com/electron/electron/blob/master/docs/api/remote.md#remote-objects
-//    */
-//   global.getReduxState = () => JSON.stringify(store.getState());
-
-//   ipcMain.on('redux-action', (event, payload) => {
-//     store.dispatch(payload);
-//   });
-// }
+ 
 
 const configureStore = (initialState) => {
   const middleware = [];
