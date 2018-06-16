c5d326cf61c18813e388e8f4c7c8ed02b3dcc90d
amatveevdev <amatveevdev9@gmail.com>
2018-06-16 15:30:41 +0300

    change configure stor

--- electron/store/configureStore.js
+++ electron/store/configureStore.js
@@ -6,25 +6,28 @@ const { routerMiddleware } = require('react-router-redux');
 const { isFSA } = require('flux-standard-action');
 
 
-const { forwardToRenderer, replayActionMain } = require('electron-redux');
-
+const {
+  forwardToRenderer,
+  triggerAlias,
+  replayActionMain
+} = require('electron-redux');
 const rootReducer = require('../reducers');
- 
 
 const configureStore = (initialState) => {
   const middleware = [];
   middleware.push(thunk);
   const router = routerMiddleware(hashHistory);
  
-  middleware.push( forwardToRenderer); // triggerAlias, 
-  // add middleware for permissions verifications
+   
+  middleware.push(triggerAlias, forwardToRenderer); // add middleware for permissions verifications
+  
 
   const enhanced = [applyMiddleware(...middleware, router)]; 
   const enhancer = compose(...enhanced);
   const store = createStore(rootReducer, initialState, enhancer);
 
   replayActionMain(store); // verification for payload, use custom electron-redux like decision
-   
+ 
   return store;
 };
 
