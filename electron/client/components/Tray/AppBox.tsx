e6defe8eca848e57109089dba2420504ed9da3f1
cypherpunk99 <amatveevdev9@gmail.com>
2018-08-08 21:41:09 +0300

    fix props

--- electron/client/components/Tray/AppBox.tsx
+++ electron/client/components/Tray/AppBox.tsx
@@ -1,12 +1,12 @@
 import * as React from "react"
 import { AppItem } from './AppItem';
 
-interface AppsListProps {
+interface AppBoxProps {
   icon: string,
   name: string
 }
 
-export class AppBox extends React.Component<AppsListProps> { 
+export class AppBox extends React.Component<AppBoxProps> { 
   public render() {
     return (
       <AppItem {...this.props}/>
