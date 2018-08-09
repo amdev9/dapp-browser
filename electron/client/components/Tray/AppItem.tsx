bdad9ba1fceadbf6c9bbbe6946fae9282b308470
cypherpunk99 <amatveevdev9@gmail.com>
2018-08-09 16:24:58 +0300

    handler add

--- electron/client/components/Tray/AppItem.tsx
+++ electron/client/components/Tray/AppItem.tsx
@@ -8,11 +8,20 @@ interface AppsItemProps {
   statusItem: Array<string>
 }
 
+interface ClickObj {
+  foo: string
+}
+
 export class AppItem extends React.Component<AppsItemProps> { 
   
   constructor(props: AppsItemProps) {
     super(props);
+    this.handleClick = this.handleClick.bind(this);
   } 
+
+  handleClick(e: Event, typeClick: ClickObj) {
+    console.log('clicked', e, typeClick.foo);
+  }
 
   public render() {
     const { icon, name, clickItem, statusItem } = this.props
@@ -29,14 +38,14 @@ export class AppItem extends React.Component<AppsItemProps> {
         </ ContextMenuTrigger>
 
         <ContextMenu id="context-menu">
-          <MenuItem data={{foo: 'bar'}} >
-          ContextMenu Item 1
+          <MenuItem data={{foo: 'pin'}} onClick={this.handleClick}>
+          Pin to top
           </MenuItem>
-          <MenuItem data={{foo: 'bar'}} >  {/* onClick={this.handleClick} */}
+          <MenuItem data={{foo: 'bar'}} onClick={this.handleClick}>
           ContextMenu Item 2
           </MenuItem>
           <MenuItem divider />
-          <MenuItem data={{foo: 'bar'}} >
+          <MenuItem data={{foo: 'bar 2'}} onClick={this.handleClick}>
           ContextMenu Item 3
           </MenuItem>
         </ContextMenu>
