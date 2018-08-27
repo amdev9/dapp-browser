c0d4341d7dbcac7771113932bd521e73c65d5122
cypherpunk99 <amatveevdev9@gmail.com>
2018-08-27 11:33:20 +0300

    fix loader epic

--- electron/main/helpers/epics/openDappEpic.ts
+++ electron/main/helpers/epics/openDappEpic.ts
@@ -1,6 +1,6 @@
 import 'rxjs'; 
 import {  ofType, Epic } from 'redux-observable';
-import { mapTo } from 'rxjs/operators';
+import { mapTo, map, mergeMap } from 'rxjs/operators';
 import { ChannelAction } from '../actions/channel';
 
 import { 
@@ -8,9 +8,20 @@ import {
   ADD_APP_ITEM
 } from '../constants';
   
+
+const addAppItem = (item: AppItem) => ({ type: ADD_APP_ITEM, payload: item });
+
+
 const openDappEpic: Epic<ChannelAction> = action$ => action$.pipe(
   ofType(TOGGLE_APP_HOME), //@todo get appName from payload
-  mapTo({ type: ADD_APP_ITEM }) // by appName resolve TrayItem
+
+  mergeMap(action =>
+    AppsManager.getAppItem(action.payload.dappName).pipe(
+      map(appItem => addAppItem(appItem))
+    )
+  )
+
+  // mapTo({ type: ADD_APP_ITEM }) // by appName resolve TrayItem
 );
 
 export default openDappEpic;
