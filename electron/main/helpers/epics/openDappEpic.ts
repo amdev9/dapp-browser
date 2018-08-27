378800c2dff76d837184e9bfb9dca2d1625cd8f3
cypherpunk99 <amatveevdev9@gmail.com>
2018-08-27 13:35:54 +0300

    todo fix epic

--- electron/main/helpers/epics/openDappEpic.ts
+++ electron/main/helpers/epics/openDappEpic.ts
@@ -1,27 +1,30 @@
 import 'rxjs'; 
 import {  ofType, Epic } from 'redux-observable';
 import { mapTo, map, mergeMap } from 'rxjs/operators';
-import { ChannelAction } from '../actions/channel';
+ 
 
 import { 
   TOGGLE_APP_HOME,
   ADD_APP_ITEM
 } from '../constants';
   
+import { AppItem, AppsManager } from '../AppsManager';
 
 const addAppItem = (item: AppItem) => ({ type: ADD_APP_ITEM, payload: item });
 
 
-const openDappEpic: Epic<ChannelAction> = action$ => action$.pipe(
+const openDappEpic: Epic<any> = action$ => action$.pipe(
   ofType(TOGGLE_APP_HOME), //@todo get appName from payload
 
-  mergeMap(action =>
-    AppsManager.getAppItem(action.payload.dappName).pipe(
-      map(appItem => addAppItem(appItem))
-    )
-  )
+  // mergeMap(action => {
+      
+  //     AppsManager.getAppItem(action.payload.dappName).pipe(
+  //       map(appItem => addAppItem(appItem))
+  //     )
+  //   }
+  // )
 
-  // mapTo({ type: ADD_APP_ITEM }) // by appName resolve TrayItem
+  mapTo({ type: ADD_APP_ITEM }) // by appName resolve TrayItem
 );
 
 export default openDappEpic;
