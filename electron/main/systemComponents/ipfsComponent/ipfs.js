6bf31ff1641dd3fd41931480683818b99564054a
cypherpunk99 <amatveevdev9@gmail.com>
2018-09-05 12:01:12 +0300

    upload success

--- electron/main/systemComponents/ipfsComponent/ipfs.js
+++ electron/main/systemComponents/ipfsComponent/ipfs.js
@@ -1,12 +1,6 @@
 const fs = require('fs');
 const IPFS = require('ipfs');
-const libp2p = require('libp2p');
-const WStar = require('libp2p-webrtc-star');
-const wrtc = require('wrtc');
-const wstar = new WStar({
-  wrtc: wrtc
-})
-
+ 
 const remoteConf = { 
   EXPERIMENTAL: {
     pubsub: true
@@ -28,7 +22,7 @@ const remoteConf = {
 };
 
 const localConf = { 
-  repo: '/Users/pidgin/dev/boilerplate/ipfsTest',
+  // repo: '/Users/pidgin/dev/boilerplate/ipfsTest',
   config: {
     Addresses: {
       API: "/ip4/127.0.0.1/tcp/5001",
@@ -36,10 +30,7 @@ const localConf = {
   }
 };
 
-const ipfs = new IPFS(localConf)
-
-
-
+const ipfs = new IPFS(localConf);
 
 ipfs.on('ready', async () => { 
   if (ipfs.isOnline()) {
@@ -49,53 +40,29 @@ ipfs.on('ready', async () => {
     ipfs.start();
   }
 
-  const version = await ipfs.version()
+  const version = await ipfs.version();
 
-  console.log('Version:', version.version)
+  console.log('Version:', version.version);
   
-  const stream = ipfs.files.addReadableStream();
-  stream.on('data', function (file) {
-    console.log(file)
-    // ipfs.files.cat(file.hash, function (err, fileContent) {
-    //   if (err) {
-    //     throw err
-    //   }
-    //   console.log(file, fileContent.toString('utf8'))
-    // });
+  var rstream = fs.createReadStream('uploadFile.ts'); 
+  const files = [
+    {
+      path: '/upload.ts',
+      content: rstream
+    }
+  ];
+  const handler = (p) => { console.log(p); };
+  const options = { 
+    progress: handler
+  };
+  ipfs.files.add(files, options, function (err, files) {
+    if(err) { console.log(err); }
+    console.log(files);
   })
-
-
-  var rstream = fs.createReadStream('/Users/pidgin/Downloads/windows.iso'); //
-  var dataLength = 0;
-  // using a readStream that we created already
-  rstream
-    .on('data', function (chunk) {
-      dataLength += chunk.length;
-      console.log('write chunk :', dataLength);
-      stream.write({
-        path: '/testFile.txt',
-        content: chunk
-      })
-    })
-    .on('end', function () {  // done
-      console.log('The length was:', dataLength);
-      stream.end();
-    });
-
- 
-  // fs.readFile('package.json', (err, data) => {
-  //   if (err) throw err;
-  //   console.log(data);
-  //   stream.write({
-  //     path: 'foo',
-  //     content: data
-  //   })
-  //   stream.end();
-  // });
 });
  
 ipfs.on('error', error => {
   console.error('Something went terribly wrong!', error)
-})
+});
 
-ipfs.on('start', () => console.log('Node started!'))
+ipfs.on('start', () => console.log('Node started!'));
--- electron/main/systemComponents/ipfsComponent/uploadFile.ts
+++ electron/main/systemComponents/ipfsComponent/uploadFile.ts
@@ -1,83 +1,76 @@
-import * as IPFS from 'ipfs';
 import * as fs from 'fs';
-import { Buffer } from 'buffer';
-import * as streamBuffers from 'stream-buffers';
- 
- 
-declare const Promise: any;
-
+import * as IPFS from 'ipfs';
 
 
 class IpfsComponent {
-  node: IPFS;
+  constructor(configuration: any) {
 
-  constructor() {
-    
-    const localConf = { 
-      repo: '/Users/pidgin/dev/boilerplate/ipfsTest',
-      config: {
-        Addresses: {
-          API: "/ip4/127.0.0.1/tcp/5001",
-        }
-      }
-    };
-    this.node = new IPFS(localConf);
   }
+  uploadFile() {}
+}
 
-  uploadIPFS = (fileArrayBuffer: ArrayBuffer): Promise<Buffer> => {
-  return new Promise((resolve: any, reject: any) => {
-
-    // this.setState({ progress: 0 });
-
-    const myReadableStreamBuffer = new streamBuffers.ReadableStreamBuffer({
-      chunkSize: 25000, 
-    });
-
-    myReadableStreamBuffer.on('data', (chunk: Buffer) => {
-      // this.setState({ progress: this.state.progress + chunk.byteLength });
-      myReadableStreamBuffer.resume();
-    });
-
-    this.stream = this.ipfs.files.addReadableStream();
-
-    this.stream.on('data', (file: Buffer) => resolve(file));
-  
-    this.stream.write(myReadableStreamBuffer);
-    myReadableStreamBuffer.put(Buffer.from(fileArrayBuffer));
-
-    myReadableStreamBuffer.on('end', () => this.stream.end());
-    myReadableStreamBuffer.stop();
-  });
-};
-
-
-    
-  const main = async () => {
-    var file = "./package.json";
-    const files: string[] = [file]; 
-    try {
-      const response = await Promise.all([...files.map(aFile  => readFile(aFile))]);
-      console.log(response);
-    } catch (err) {
-      console.log(err);
+const remoteConf = { 
+  EXPERIMENTAL: {
+    pubsub: true
+  },
+  config: {
+    Bootstrap: [
+      "/ip4/35.204.17.104/tcp/4001/ipfs/QmWCsxqpvYMKCeCejvXLc7TbWrraLwmAKMxWgcsKQ8xUL3"
+    ],
+    Addresses: {
+      Swarm: [
+        "/ip4/0.0.0.0/tcp/4001",
+        "/ip6/::/tcp/4001",
+        "/dns4/discovery.libp2p.array.io/tcp/9091/wss/p2p-websocket-star/"
+      ],
+      API: "/ip4/127.0.0.1/tcp/5001",
+      Gateway: "/ip4/127.0.0.1/tcp/8080"
     }
   }
+};
 
-}
+const localConf = { 
+  // repo: '/Users/pidgin/dev/boilerplate/ipfsTest',
+  config: {
+    Addresses: {
+      API: "/ip4/127.0.0.1/tcp/5001",
+    }
+  }
+};
 
-async function readFileAsync(path: string, opts = 'utf8') {
-  return new Promise((res: any, rej: any) => {
-    fs.readFile(path, opts, (err: Error, data: any) => {
-      if (err) rej(err)
-      else res(data)
-    })
+const ipfs = new IPFS(localConf);
+
+ipfs.on('ready', async () => { 
+  if (ipfs.isOnline()) {
+    console.log('online');
+  } else {
+    console.log('offline, try to start');
+    ipfs.start();
+  }
+
+  const version = await ipfs.version();
+
+  console.log('Version:', version.version);
+  
+  var rstream = fs.createReadStream('uploadFile.ts'); 
+  const files = [
+    {
+      path: '/upload.ts',
+      content: rstream
+    }
+  ];
+  const handler = (p) => { console.log(p); };
+  const options = { 
+    progress: handler
+  };
+  ipfs.files.add(files, options, function (err, files) {
+    if(err) { console.log(err); }
+    console.log(files);
   })
-}
+});
+ 
+ipfs.on('error', error => {
+  console.error('Something went terribly wrong!', error)
+});
 
-
-const readFile = async (file: any) => { //@todo fix to pipe object
-  let data = await readFileAsync('package.json');
-  uploadIPFS(data);  
-}
-
-main();
+ipfs.on('start', () => console.log('Node started!'));
