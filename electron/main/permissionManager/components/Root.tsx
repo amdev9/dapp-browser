027b17a535fef57ec8eb63cd50df4a00c3a3a70f
cypherpunk99 <amatveevdev9@gmail.com>
2018-09-11 11:45:03 +0300

    add permissions to electronmanager

---
+++ electron/main/permissionManager/components/Root.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Store } from 'redux';
+import { Provider } from 'react-redux';
+import App from '../components/App';
+import { PermissionLayout } from '../components/PermissionLayout';
+import { electronManager } from '../permissions';
+
+interface IRootType {
+  store: Store<any>; // Redux.Store<any>;
+};
+
+export default function Root({ store }: IRootType) {
+  return (
+    <Provider store={store}> 
+      <App>
+        <PermissionLayout permissions={electronManager.permissions}/>
+      </App>
+    </Provider>
+  );
+}
