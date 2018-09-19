d1d2f2608555fe8bf9ae802eecaa904217173605
dtaipov@gmail.com <dtaipov@gmail.com>
2018-09-19 20:09:05 +0300

    #99 Search. 1) Remove Search input toggling. It should be always opened. 2) Search results should take the whole screen width, not only the width under the input

--- electron/client/components/HeaderBar/SuggestSearch/index.tsx
+++ electron/client/components/HeaderBar/SuggestSearch/index.tsx
@@ -4,8 +4,6 @@ import {SearchItem} from "../../../redux/model";
 
 // Assets
 const searchIcon = require("../../../assets/icons/search.svg");
-const closeIcon = require("../../../assets/icons/close.svg");
-
 
 export interface SuggestSearchProps {
   searchItems: { [index: string]: SearchItem[] }
@@ -22,45 +20,13 @@ export interface SuggestSearchState {
 }
 
 export class SuggestSearch extends React.Component<SuggestSearchProps, SuggestSearchState> {
-  constructor(props: SuggestSearchProps) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-  }
-
-  public state: SuggestSearchState = {
-    isOpen: false,
-  };
-
-  private toggle() {
-    const { isOpen } = this.state;
-
-    // Hide or show title in headerbar by searchbar status
-    const title = document.querySelector(".headerbar .title") as HTMLElement;
-    if (title) {
-      const willOpen = !isOpen;
-      if (willOpen) {
-        title.style.setProperty("--headerbar-title-display", "none")
-      } else {
-        title.style.setProperty("--headerbar-title-display", "block")
-      }
-    }
-
-    // Toggle And clear
-    this.setState({
-      isOpen: !isOpen,
-    })
-  }
 
   public render() {
-    const { isOpen } = this.state;
     const { searchItems, togglePanel, toggleStatusBar, isStatusBarOpen, isPeersBarOpen, togglePeersBar} = this.props;
-    const actionIcon = isOpen ? closeIcon : searchIcon;
-    const visibleClass = isOpen ? "showed" : "hidden";
 
     return (
       <div className="search">
-        <div className={`search-content ${visibleClass}`}>
+        <div className={`search-content`}>
           <div className="title">
             URI:
           </div>
@@ -73,8 +39,8 @@ export class SuggestSearch extends React.Component<SuggestSearchProps, SuggestSe
             togglePeersBar={togglePeersBar}
           />
         </div>
-        <div className="action" onClick={this.toggle}>
-          <img className={visibleClass} src={actionIcon} />
+        <div className="action">
+          <img src={searchIcon} />
         </div>
       </div>
     )
