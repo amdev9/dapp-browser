2de66e12de9ee68b3d2680be42b11c1b4f7be7c5
Sozinov Alexander <sanzeting@mail.ru>
2018-10-19 19:04:55 +0300

    IpfsRoom actions

--- electron/main/helpers/actions/ipfsRoom.ts
+++ electron/main/helpers/actions/ipfsRoom.ts
@@ -1,6 +1,6 @@
 import { action } from 'typesafe-actions';
 import * as constants from '../constants';
 
-export const ipfsRoomSubscribeSuccess = () => action(constants.IPFS_ROOM_SUBSCRIBE_SUCCESS)
+export const ipfsRoomSubscribeSuccess = (targetUUID: string) => action(constants.IPFS_ROOM_SUBSCRIBE_SUCCESS)
 
-export const ipfsRoomSubscribeFailure = () => action(constants.IPFS_ROOM_SUBSCRIBE_FAILURE)
+export const ipfsRoomSubscribeFailure = (error: string, targetUUID: string) => action(constants.IPFS_ROOM_SUBSCRIBE_FAILURE)
