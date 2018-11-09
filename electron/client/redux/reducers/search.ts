8e74659502deac6d04b68a1a6668304f4935cf57
dtaipov@gmail.com <dtaipov@gmail.com>
2018-11-09 11:43:40 +0300

    #188 init client state in Main

--- electron/client/redux/reducers/search.ts
+++ electron/client/redux/reducers/search.ts
@@ -5,43 +5,43 @@ import * as searchActions from '../actions/search';
 
 export type SearchAction = ActionType<typeof searchActions>;
 
-const initialState: SearchPanel =
-  {items:
-    {
-    applications: [
-      {
-        icon: require("../../assets/app-icons/chat.svg"),
-        uri: "chat://sendMessage/satan/",
-        network: "mainnet",
-        app: "Chat1",
-      },
-      {
-        icon: require("../../assets/app-icons/exchange.svg"),
-        uri: "exchange://send-file/",
-        network: "testnet",
-        app: "Exchange",
-      },
-    ],
+// const initialState: SearchPanel =
+//   {items:
+//     {
+//     applications: [
+//       {
+//         icon: require("../../assets/app-icons/chat.svg"),
+//         uri: "chat://sendMessage/satan/",
+//         network: "mainnet",
+//         app: "Chat1",
+//       },
+//       {
+//         icon: require("../../assets/app-icons/exchange.svg"),
+//         uri: "exchange://send-file/",
+//         network: "testnet",
+//         app: "Exchange",
+//       },
+//     ],
 
-    marketplace: [
-      {
-        icon: require("../../assets/app-icons/chat.svg"),
-        uri: "chat://marketplace/install?name=Chat",
-        network: "mainnet",
-        app: "Chat",
-      },
-      {
-        icon: require("../../assets/app-icons/exchange.svg"),
-        uri: "chat://marketplace/install?name=Exchange",
-        network: "testnet",
-        app: "Exchange",
-      },
-    ]},
+//     marketplace: [
+//       {
+//         icon: require("../../assets/app-icons/chat.svg"),
+//         uri: "chat://marketplace/install?name=Chat",
+//         network: "mainnet",
+//         app: "Chat",
+//       },
+//       {
+//         icon: require("../../assets/app-icons/exchange.svg"),
+//         uri: "chat://marketplace/install?name=Exchange",
+//         network: "testnet",
+//         app: "Exchange",
+//       },
+//     ]},
 
-    isOpen: false
-  };
+//     isOpen: false
+//   };
 
-export default function search(state: SearchPanel = initialState, action: SearchAction) {
+export default function search(state: SearchPanel = null, action: SearchAction) {
   switch (action.type) {
     case TOGGLE_SEARCH_PANEL:
       if (action.payload && action.payload.hasOwnProperty('isOpen')) {
