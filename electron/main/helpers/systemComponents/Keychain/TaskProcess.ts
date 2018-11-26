dd067b0f5c61c545db36e05862d3d7f5ee297424
cypherpunk99 <amatveevdev9@gmail.com>
2018-11-26 16:04:14 +0300

    refactor keychain system component

---
+++ electron/main/helpers/systemComponents/Keychain/TaskProcess.ts
@@ -0,0 +1,76 @@
+import { EOL } from 'os';
+import { Queue, Task } from './Queue';
+
+export const MAX_RETRIES = 3;
+
+class TaskProcess {
+  public task: Task;
+  public retries: number;
+  private queue: Queue;
+
+  constructor(queue: Queue, task: Task) {
+    this.retries = MAX_RETRIES;
+    this.queue = queue;
+    this.task = task;
+  }
+
+  public async run(): Promise<any> {
+
+    const { stdin, stdout } = this.queue.keychain;
+    const { keychain } = this.queue;
+
+    return new Promise(async (resolve, reject) => {
+      keychain.on('close', (code: number, signal: string) => {
+        // self clean
+        cleanup();
+        let error: Error;
+        if (code !== null) {
+          this.retries = this.retries - 1;
+          error = new Error(`keychain process exited with code (retry ${MAX_RETRIES - this.retries}): ${code}`);
+        } else {
+          this.retries = 0;
+          error = new Error(`keychain process abnormally terminated by signal: ${signal}`);
+        }
+
+        (error as any).normal = false;
+        reject(error);
+      });
+
+      const cleanup = () => {
+        stdout.removeListener('data', handleResult);
+        stdout.removeListener('error', handleError);
+        stdin.removeListener('error', handleError);
+
+        keychain.removeAllListeners();
+      };
+
+      const handleError = (error: Error) => {
+        (error as any).normal = true;
+        cleanup();
+        reject(error);
+      };
+
+      const handleResult = (response: string) => {
+        const deserialized = JSON.parse(response);
+
+        if (deserialized.error) {
+          handleError(new Error(deserialized.error));
+        } else {
+          cleanup();
+          resolve(deserialized.result);
+        }
+      };
+
+      stdout.addListener('data', handleResult);
+      stdout.addListener('error', handleError);
+      stdin.addListener('error', handleError);
+
+      const serialized = JSON.stringify(this.task);
+      await stdin.write(serialized);
+      await stdin.write(EOL);
+      stdin.end();
+    });
+  }
+}
+
+export { TaskProcess };
--- electron/main/helpers/systemComponents/Keychain/queue.ts
+++ electron/main/helpers/systemComponents/Keychain/queue.ts
@@ -1,10 +1,7 @@
 import { ChildProcess, spawn } from 'child_process';
-import { EOL } from 'os';
+import { TaskProcess } from './TaskProcess';
 
-export const RESPONSE_TIMEOUT = 1000;
-export const MAX_RETRIES = 3;
-
-export interface Task {
+interface Task {
   command: 'CMD_CREATE' | 'CMD_SIGN' | 'CMD_LIST';
   params?: any;
   promise: {
@@ -13,77 +10,7 @@ export interface Task {
   };
 }
 
-export class TaskProcess {
-  public task: Task;
-  public retries: number;
-  private queue: Queue;
-
-  constructor(queue: Queue, task: Task) {
-    this.retries = MAX_RETRIES;
-    this.queue = queue;
-    this.task = task;
-  }
-
-  public async run(): Promise<any> {
-    return new Promise(async (resolve, reject) => {
-      const { stdin, stdout } = this.queue.keychain;
-      const { keychain } = this.queue;
-
-      keychain.on('close', (code: number, signal: string) => {
-        // self clean
-        cleanup();
-
-        let error: Error;
-        if (code !== null) {
-          this.retries--;
-          error = new Error(`keychain process exited with code (retry ${MAX_RETRIES - this.retries}): ${code}`);
-        } else {
-          this.retries = 0;
-          error = new Error(`keychain process abnormally terminated by signal: ${signal}`);
-        }
-
-        (error as any).normal = false;
-        reject(error);
-      });
-
-      const cleanup = () => {
-        stdout.removeListener('data', handleResult);
-        stdout.removeListener('error', handleError);
-        stdin.removeListener('error', handleError);
-
-        keychain.removeAllListeners();
-      };
-
-      const handleError = (error: Error) => {
-        (error as any).normal = true;
-        cleanup();
-        reject(error);
-      };
-
-      const handleResult = (response: string) => {
-        const deserialized = JSON.parse(response);
-
-        if (deserialized.error) {
-          handleError(new Error(deserialized.error));
-        } else {
-          cleanup();
-          resolve(deserialized.result);
-        }
-      };
-
-      stdout.addListener('data', handleResult);
-      stdout.addListener('error', handleError);
-      stdin.addListener('error', handleError);
-
-      const serialized = JSON.stringify(this.task);
-      await stdin.write(serialized);
-      await stdin.write(EOL);
-      stdin.end();
-    });
-  }
-}
-
-export class Queue {
+class Queue {
   private activeProcess?: TaskProcess;
   public keychain: ChildProcess;
   private queue: Task[];
@@ -137,3 +64,5 @@ export class Queue {
     }
   }
 }
+
+export { Task, Queue };
