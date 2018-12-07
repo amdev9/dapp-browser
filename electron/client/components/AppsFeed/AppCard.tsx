c062ce4bea6081b84ed1233edbad92a47d3ddd57
dtaipov@gmail.com <dtaipov@gmail.com>
2018-12-07 16:29:42 +0300

    #204 install link added to AppCard

--- electron/client/components/AppsFeed/AppCard.tsx
+++ electron/client/components/AppsFeed/AppCard.tsx
@@ -1,16 +1,25 @@
-import * as React from "react"
+import { MoonLoader } from 'react-spinners';
+import * as React from 'react';
 import { DApp } from '../../redux/model';
 // import { MoonLoader } from "react-spinners";
 
 interface AppCardProps {
-  dapp?: DApp,
+  dapp?: DApp;
   switchDapp: () => void;
 }
 
-export class AppCard extends React.Component<AppCardProps>  {
+interface AppCardState {
+  status: string;
+}
+
+export class AppCard extends React.Component<AppCardProps, AppCardState>  {
   constructor(props: AppCardProps) {
     super(props);
     this.getCategories = this.getCategories.bind(this);
+    this.actionHandle = this.actionHandle.bind(this);
+    this.state = {
+      status: '',
+    };
   }
 
   private getCategories(): JSX.Element {
@@ -26,6 +35,39 @@ export class AppCard extends React.Component<AppCardProps>  {
       </div>
     );
   }
+
+  private async actionHandle(e: React.MouseEvent<HTMLDivElement>) {
+    e.stopPropagation();
+    // @TODO: add functionaly cb wrapper here\
+    this.setState({
+      status: 'install',
+    });
+  }
+
+  private getAction(): JSX.Element {
+    const { status } = this.state;
+
+    const label = (
+      <div className="label">Intall</div>
+    );
+
+    // @TODO: next add spinner type by status
+    // e.g for updated or install indicator is progress
+    const spinner = (
+      <div className="loading">
+        <MoonLoader color="#508dff" size={13} />
+      </div>
+    );
+
+    const content = status ? spinner : label;
+
+    return (
+      <div className="action" onClick={this.actionHandle}>
+        {content}
+      </div>
+    );
+  }
+
   public render() {
     const { dapp, switchDapp } = this.props;
     return (
@@ -36,6 +78,7 @@ export class AppCard extends React.Component<AppCardProps>  {
           <div className="title">{dapp.appName}</div>
           <div className="footer">
             {this.getCategories()}
+            {this.getAction()}
           </div>
         </div>
       </div>
